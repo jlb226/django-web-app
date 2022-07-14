015a4d7b0b016b9d8155f4c77bb3f401b8c6980f
Jay <jlb226@georgetown.edu>
2022-07-13 20:36:57 -0400

    Added signin, signup, and signout functions
    Added signin, signup, and signout functions

--- app/views.py
+++ app/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . import views
 
 
 # Create your views here.
 def home(request):
   return HttpResponse("Hello")
 
+
+def signup(request):
+  return render(request, "app/signup.html")
+
+
+def signin(request):
+  return render(request, "app/signin.html")
+
+
+def signout(request):
+  pass
