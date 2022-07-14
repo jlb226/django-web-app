fc108381fbd3aad2fbcf32b4f9a3d135b67e84fb
Jay <jlb226@georgetown.edu>
2022-07-14 10:32:58 -0400

    Removed signup
    Removed signup

--- app/views.py
+++ app/views.py
@@ -17,74 +17,74 @@ def home(request):
   return render(request, "app/index.html")
 
 
-def signup(request):
-  
-  if request.method == "POST":
-    username = request.POST['username']
-    fname = request.POST['fname']
-    lname = request.POST['lname']
-    email = request.POST['email']
-    pass1 = request.POST['pass1']
-    pass2 = request.POST['pass2']
-    
-    if User.objects.filter(username=username):
-      messages.error(request, "Username already exists")
-      return redirect("home")
-      
-    if User.objects.filter(email=email):
-      messages.error(request, "Email is already registered")
-      return redirect("home")
-    
-    if len(username)>24:
-      messages.error(request, "Username must be shorter than 25 characters")
-      
-    if pass1 != pass2:
-      messages.error(request, "Passwords do not match")
-      
-    if not username.isalnum():
-      messages.error(request, "Username must be alphanumeric")
-      return redirect('home')
-    
-    myuser = User.objects.create_user(username, email, pass1)
-    myuser.first_name = fname
-    myuser.last_name = lname
-    myuser.is_active = False
-    myuser.save()
-    
-    messages.success(request, "Your account has been successfully created.")
-    
-    ## Welcome email
-    
-    subject = "Welcome to the Django Web App"
-    message = "Hello, " + myuser.first_name + "\n" + "Welcome to the Django Web App."
-    from_email = settings.EMAIL_HOST_USER
-    to_list = [myuser.email]
-    send_mail(subject, message, from_email, to_list, fail_silently=True)
-    
-    ## Confirmation email
-    
-    current_site = get_current_site(request)
-    email_subject = "Confirm your email address"
-    message2 = render_to_string('email_confirmation.html', {
-     'name': myuser.first_name,
-     'domain': current_site.domain,
-     'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
-     'token': generate_token.make_token(myuser)
-    })
-    
-    email = EmailMessage(
-     email_subject,
-     message2,
-     settings.EMAIL_HOST_USER,
-     [myuser.email]
-    )
-    
-    email.fail_silently = True
-    email.send()
-    
-    return redirect('signin')
-    
-  return render(request, "app/signup.html")
+#def signup(request):
+#  
+#  if request.method == "POST":
+#    username = request.POST['username']
+#    fname = request.POST['fname']
+#    lname = request.POST['lname']
+#    email = request.POST['email']
+#    pass1 = request.POST['pass1']
+#    pass2 = request.POST['pass2']
+#    
+#    if User.objects.filter(username=username):
+#      messages.error(request, "Username already exists")
+#      return redirect("home")
+#      
+#    if User.objects.filter(email=email):
+#      messages.error(request, "Email is already registered")
+#      return redirect("home")
+#    
+#    if len(username)>24:
+#      messages.error(request, "Username must be shorter than 25 characters")
+#      
+#    if pass1 != pass2:
+#      messages.error(request, "Passwords do not match")
+#      
+#    if not username.isalnum():
+#      messages.error(request, "Username must be alphanumeric")
+#      return redirect('home')
+#    
+#    myuser = User.objects.create_user(username, email, pass1)
+#    myuser.first_name = fname
+#    myuser.last_name = lname
+#    myuser.is_active = False
+#    myuser.save()
+#    
+#    messages.success(request, "Your account has been successfully created.")
+#    
+#    ## Welcome email
+#    
+#    subject = "Welcome to the Django Web App"
+#    message = "Hello, " + myuser.first_name + "\n" + "Welcome to the Django Web App."
+#    from_email = settings.EMAIL_HOST_USER
+#    to_list = [myuser.email]
+#    send_mail(subject, message, from_email, to_list, fail_silently=True)
+#    
+#    ## Confirmation email
+#    
+#    current_site = get_current_site(request)
+#    email_subject = "Confirm your email address"
+#    message2 = render_to_string('email_confirmation.html', {
+#     'name': myuser.first_name,
+#     'domain': current_site.domain,
+#     'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+#     'token': generate_token.make_token(myuser)
+#    })
+#    
+#    email = EmailMessage(
+#     email_subject,
+#     message2,
+#     settings.EMAIL_HOST_USER,
+#     [myuser.email]
+#    )
+#    
+#    email.fail_silently = True
+#    email.send()
+#    
+#    return redirect('signin')
+#    
+#  return render(request, "app/signup.html")
 
 
 def signin(request):
