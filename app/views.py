333a4e4d6bc5b806059b0a8aca6ebc4e7d8c55c8
Jay <jlb226@georgetown.edu>
2022-07-14 11:16:28 -0400

    Update views.py

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ from app_platform import settings
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_bytes
 from . tokens import generate_token
 
 
