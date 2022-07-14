c02f698bc7e8daa55be38d7c8f04cebfdd3abea5
Jay <jlb226@georgetown.edu>
2022-07-14 00:24:47 -0400

    Update tokens.py

--- app/tokens.py
+++ app/tokens.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.token import PasswordResetTokenGenerator
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 from six import text_type
 class TokenGenerator(PasswordResetTokenGenerator):
