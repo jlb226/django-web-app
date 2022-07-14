da8a4a80b031c6b1c240c4a42e354b4393301177
Jay <jlb226@georgetown.edu>
2022-07-13 23:07:40 -0400

    Update views.py

--- app/views.py
+++ app/views.py
@@ -13,7 +13,6 @@ def home(request):
 def signup(request):
   
   if request.method == "POST":
-    #username = request.POST.get('username')
     username = request.POST['username']
     fname = request.POST['fname']
     lname = request.POST['lname']
@@ -21,7 +20,7 @@ def signup(request):
     pass1 = request.POST['pass1']
     pass2 = request.POST['pass2']
     
-    myuser = User.objects.create(username, email, pass1)
+    myuser = User.objects.create_user(username, email, pass1)
     myuser.first_name = fname
     myuser.last_name = lname
     
