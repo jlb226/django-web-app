56ffc7cf2fdfbf442d4f96c096478e2f712e7911
Jay <jlb226@georgetown.edu>
2022-07-13 23:06:31 -0400

    Expanded signup function behavior
    Expanded signup function behavior

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 # Create your views here.
@@ -8,6 +11,26 @@ def home(request):
 
 
 def signup(request):
+  
+  if request.method == "POST":
+    #username = request.POST.get('username')
+    username = request.POST['username']
+    fname = request.POST['fname']
+    lname = request.POST['lname']
+    email = request.POST['email']
+    pass1 = request.POST['pass1']
+    pass2 = request.POST['pass2']
+    
+    myuser = User.objects.create(username, email, pass1)
+    myuser.first_name = fname
+    myuser.last_name = lname
+    
+    myuser.save()
+    
+    messages.success(request, "Your account has been successfully created.")
+    
+    return redirect('signin')
+    
   return render(request, "app/signup.html")
 
 
